ccdb84311449de8b4b506defa121886fa0b8ea66
Brendan Murray <bpmurray@users.noreply.github.com>
2016-03-05 13:16:19 +0000

    Update ds18b20-node.js
    Added a mechanism whereby setting msg.family to a value causes the directory name to change as appropriate to the correct value for the sensor family. The current set of families is "28" for DS18B20, "10" for DS18S20. It makes more sense to search /sys/devices/w1_bus_master1/w1_master_slaves file for the list of devices. However, this should provide a simple temporary solution.

--- ds18b20-node/ds18b20-node.js
+++ ds18b20-node/ds18b20-node.js
@@ -33,7 +33,11 @@ module.exports = function(RED) {
 
       // The root path of the sensors
       var W1PATH = "/sys/bus/w1/devices";
-
+      // The devices list file
+      var W1DEVICES = "/sys/devices/w1_bus_master1/w1_master_slaves";
+      // Device family: "28"= DS18B20, "10" = DS18S20
+      var family = "28";
+      
       // Save the default device ID
       this.topic = config.topic;
 
@@ -48,7 +52,7 @@ module.exports = function(RED) {
          // Get all the IDs - extra work, but that's OK
          for (var iX=0; iX<dirList.length; iX++) {
             var dir = dirList[iX].toUpperCase();
-            if (dir.indexOf("28-") == 0 && dir.length == 15) {
+            if (dir.indexOf(family+"-") == 0 && dir.length == 15) {
                // Format the name
                dirList[iX] = dir.substr(13,2)+dir.substr(11,2)+dir.substr(9,2)
                            + dir.substr(7,2)+dir.substr(5,2)+dir.substr(3,2);
@@ -78,7 +82,7 @@ module.exports = function(RED) {
             if (dev !== null && dev.length == 12) {
                var dir = dev.substr(10,2) + dev.substr(9,2) + dev.substr(6,2)
                        + dev.substr(4,2) + dev.substr(2,2) + dev.substr(0,2);
-               var fData = fs.readFileSync(W1PATH + "/28-" + dir.toLowerCase()
+               var fData = fs.readFileSync(W1PATH + "/" + family+ "-" + dir.toLowerCase()
                                            + "/w1_slave", fsOptions).trim();
                var tBeg = fData.indexOf("t=")+2;
                var tEnd = tBeg+1;
@@ -105,6 +109,9 @@ module.exports = function(RED) {
       this.on('input', function (msg) {
          if (msg.topic !== undefined && msg.topic !== "") {
             this.topic = msg.topic;
+         }
+         if (msg.family !== undefined && msg.family !== "") {
+            this.family = msg.family;
          }
          var arr = this.read(msg);
          
